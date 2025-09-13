a113de5420279b717d15a9326996845d5fc491ac
Braden Griebel <bgriebel@uw.edu>
2025-09-13 09:17:07 -0700

    typo fix

--- modules/home-manager/coding/default.nix
+++ modules/home-manager/coding/default.nix
@@ -8,5 +8,5 @@
   # With the imported modules can then use below to set default
   # enabled modules
   python.enable = lib.mkDefault true;
-  typst.enable = lib.mlDefault true;
+  typst.enable = lib.mkDefault true;
 }
